e0c1c86276af571c370a45cf7b521b7d5ece4f8a
beevabeeva <avibank1@gmail.com>
2019-07-22 21:15:13 +0200

    Updated hello command to run git stuff

--- utt/commands/hello.py
+++ utt/commands/hello.py
@@ -1,11 +1,21 @@
 from ..entry import Entry
-
+from os import system
+import sys
 
 class HelloHandler:
     def __init__(self, args, now, add_entry):
         self._args = args
         self._now = now
         self._add_entry = add_entry
+        git="git -C /home/avi/Documents/utt_git/utt_git/ pull -q;"
+        print("getting latest version of utt.log")
+        system(git)
+        # DANGER DANGER DANGER
+        # WARNING: this overwrites log file
+        update="cat /home/avi/Documents/utt_git/utt_git/out.txt > ~/.local/share/utt/utt.log"
+        # print(run)        # for testing
+        system(update)
+        print("done")
 
     def __call__(self):
         self._add_entry(Entry(self._now, 'hello', False))
